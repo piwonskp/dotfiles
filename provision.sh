ec07b6ec14c6c1d6f88743ccce343f1306b28ace
Piotr Piwoński <piwonskp@gmail.com>
2017-09-26 23:31:38 +0200

    Add thunar and gtk themes

--- provision.sh
+++ provision.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
+$USERNAME = $1
+$HOME = "/home/$USERNAME"
+
 pacman -S - < pkglist.txt
 
 pip install websocket-client
 pip install requests
 
-mkdir ~/.config/termite
+mkdir $HOME/.config/termite
 
-ln -s ~/dotfiles/.Xresources ~/.Xresources
-ln -s ~/dotfiles/.i3 ~/.i3
-ln -s ~/dotfiles/.config/i3blocks ~/.config/i3blocks
-ln -s ~/dotfiles/.config/termite ~/.config/termite
+ln -s $HOME/dotfiles/.Xresources $HOME/.Xresources
+ln -s $HOME/dotfiles/.i3 $HOME/.i3
+ln -s $HOME/dotfiles/.config/i3blocks $HOME/.config/i3blocks
+ln -s $HOME/dotfiles/.config/termite $HOME/.config/termite
+ln -s $HOME/dotfiles/.gtkrc-2.0 $HOME/.gtkrc-2.0
+ln -s $HOME/dotfiles/.config/gtk-3.0/settings.ini $HOME/.config/gtk-3.0/settings.ini
 
 timedatectl set-timezone Europe/Warsaw
 localectl set-locale LANG=pl_PL.utf8
