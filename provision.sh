6328ba4c74b640baf9763bb872a1f0430577fa71
Piotr Piwoński <piwonskp>
2025-02-17 22:39:15 +0100

    link gtk-3.0 directory

--- provision.sh
+++ provision.sh
@@ -2,7 +2,7 @@
 
 ln -s $HOME/dotfiles/.Xresources $HOME/.Xresources
 ln -s $HOME/dotfiles/.gtkrc-2.0 $HOME/.gtkrc-2.0
-ln -s $HOME/dotfiles/.config/gtk-3.0/settings.ini $HOME/.config/gtk-3.0/settings.ini
+ln -s $HOME/dotfiles/.config/gtk-3.0/settings.ini $HOME/.config/gtk-3.0
 ln -s $HOME/dotfiles/.config/sway $HOME/.config/sway
 ln -s $HOME/dotfiles/.config/waybar $HOME/.config/waybar
 ln -s $HOME/dotfiles/.config/rofi $HOME/.config/rofi
