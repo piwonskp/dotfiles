64d77e9078bbf9a5ea17b4f11e811b4a7ceed626
Piotr Piwoński <piwonskp@gmail.com>
2025-03-05 06:51:28 +0100

    Add GitHub notifications module to Waybar; enhance Sway config with new keybindings and floating window settings

---
+++ .config/waybar/github.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+token=`cat ${HOME}/.config/github/notifications.token`
+count=`curl -u username:${token} https://api.github.com/notifications | jq '. | length'`
+
+
+if [[ "$count" == "0" ]]; then
+    echo '{"text":""}'
+else
+    echo '{"text":'$count',"tooltip":"$tooltip","class":"active"}'
+fi
