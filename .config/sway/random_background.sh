e9e57b6770b2aa4b0aec3746306f24a89c436685
Piotr Piwoński <piwonskp@gmail.com>
2025-05-04 14:27:05 +0200

    Fix random background script to prevent selecting the directory itself

--- .config/sway/random_background.sh
+++ .config/sway/random_background.sh
@@ -1,1 +1,1 @@
-find /usr/share/backgrounds/ -path /usr/share/backgrounds/sway -prune -o -print | shuf -n 1
+find /usr/share/backgrounds/ -type f ! -path "/usr/share/backgrounds/sway/*" -print | shuf -n 1
